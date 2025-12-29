5cd8baa06cd9c6ce5cd1fbb044834ddce1c6293b
Erly123 <choquelayme@gmail.com>
2025-12-29 17:58:33 -0300

    first commit

---
+++ src/main/java/ch/l/jbank/controller/HelloController.java
@@ -0,0 +1,14 @@
+package ch.l.jbank.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public ResponseEntity<String> ok() {
+        System.out.println("Inside Controler..");
+        return ResponseEntity.ok().build();
+    }
+}
