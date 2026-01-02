e07c95bfaf006f109bd169f016daa5c179a8185d
Erly123 <choquelayme@gmail.com>
2026-01-02 15:59:37 -0300

    feat: implement money deposit functionality

--- src/main/java/ch/l/jbank/controller/WalletController.java
+++ src/main/java/ch/l/jbank/controller/WalletController.java
@@ -1,12 +1,15 @@
 package ch.l.jbank.controller;
 
 import ch.l.jbank.controller.dto.CreateWalletDto;
+import ch.l.jbank.controller.dto.DepositMoneyDto;
 import ch.l.jbank.service.WalletService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/wallets")
@@ -24,6 +27,30 @@ public class WalletController {
         var wallet = walletService.createWallet(dto);
 
         return ResponseEntity.created(URI.create("/wallets/" + wallet.getWalletId().toString())).build();
+    }
+
+    @DeleteMapping(path = "/{walletId}")
+    public ResponseEntity<Void> deleteWallet(@PathVariable("walletId") UUID walletId) {
+
+        var deleted = walletService.deleteWallet(walletId);
+
+        return deleted ?
+                ResponseEntity.noContent().build() :
+                ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(path = "/{walletId}/deposits")
+    public ResponseEntity<Void> depositMoney(@PathVariable("walletId") UUID walletId,
+                                             @RequestBody @Valid DepositMoneyDto dto, HttpServletRequest servletRequest) {
+
+        walletService.depositMoney(
+                walletId,
+                dto,
+                servletRequest.getAttribute("x-user-ip").toString()
+        );
+        return ResponseEntity.ok().build();
+
+
     }
 
 }
---
+++ src/main/java/ch/l/jbank/controller/dto/DepositMoneyDto.java
@@ -0,0 +1,9 @@
+package ch.l.jbank.controller.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record DepositMoneyDto(@NotNull @DecimalMin("10.00") BigDecimal value) {
+}
