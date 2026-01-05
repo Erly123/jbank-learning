80aff323078b58d7cf9b36aec575bb8dc4722573
Erly123 <choquelayme@gmail.com>
2026-01-05 16:33:04 -0300

    feat: add statement and pagination DTOs

---
+++ src/main/java/ch/l/jbank/controller/dto/PaginationDto.java
@@ -0,0 +1,7 @@
+package ch.l.jbank.controller.dto;
+
+public record PaginationDto(Integer page,
+                            Integer pageSize,
+                            Long totalElements,
+                            Integer totalPages) {
+}
